f571c6a642b832f5e9277b9bffe92275bb8e0ee3
homer <homer@pop-os.localdomain>
2021-02-24 19:39:43 +0100

    modification mesh.py

--- triangle.py
+++ triangle.py
@@ -7,12 +7,12 @@ import point
 class Triangle:
     """Un simple triangle."""
 
-    _counter = 0
+    _counter = 1
     _name = "Triangle"
 
     def __init__(self, points, physical_tag=-1):
-        self.id = Trangle._counter
-        Triangle_counter += 1
+        self.id = Triangle._counter
+        Triangle._counter += 1
         self.physical_tag = physical_tag
         assert len(points) == 3, "A triangle must have 3 points"
         self.points = points
@@ -20,12 +20,12 @@ class Triangle:
         self._set_area()
 
     def _set_area(self):
-        x0 = self.points[0][0]
-        y0 = self.points[0][1]
-        x1 = self.points[1][0]
-        y1 = self.points[1][1]
-        x2 = self.points[2][0]
-        y2 = self.points[2][1]
+        x0 = self.points[0].X[0]
+        y0 = self.points[0].X[1]
+        x1 = self.points[1].X[0]
+        y1 = self.points[1].X[1]
+        x2 = self.points[2].X[0]
+        y2 = self.points[2].X[1]
         self._area = abs((x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0))
 
     def area(self):
